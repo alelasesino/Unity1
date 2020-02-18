36434b5626e177e5d23ed213c7680923582e75d4
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-02-19 00:12:45 +0100

    Version 2 - 18/02/2020

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed;
+    public Text countText;
+    public Text winText;
+    public Button btRestart;
+
     private Rigidbody rb;
+    private int count;
+    private bool isWin;
 
     void Start() {
     
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+        isWin = false;
+        btRestart.gameObject.SetActive(false);
 
     }
 
     void FixedUpdate() {
     
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        if(!isWin) {
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movement * speed);
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+            rb.AddForce(movement * speed);
+
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+    
+        if(other.gameObject.CompareTag("Pick Up")) {
+        
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+            checkWin();
+        }
+
+    }
+    
+    void setCountText() {
+
+        countText.text = "Puntos: " + count.ToString() + "/12";
+
+    }
+
+    void checkWin() {
+    
+        isWin = count >=12;
+
+        if(isWin) {
+        
+            winText.text = "¡Enhorabuena has ganado!";
+            btRestart.gameObject.SetActive(true);
+        }
+    
+    }
+
+    public void onRestartClick() {
+
+        winText.text = "";
+        transform.position = new Vector3(0.0f,0,0);
+        isWin = false;
+        count = 0;
+        setCountText();
+        btRestart.gameObject.SetActive(false);
 
     }
     
