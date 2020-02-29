a453ae136c048b61d4d2108c5acc527478774fe3
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-02-29 15:01:59 +0100

    Added Fall Damage

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,15 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
 
+    public bool grounded;
+    public float fallDamagePerSecond;
+    public float minFallTime;
+    public float currentFallTime;
+    public float currentHealth;
+
+
     public float speed;
-    public Text countText, winText;
+    public Text countText, winText, healthText;
     public Button btRestart;
 
     private const int MAX_POINTS = 60;    
@@ -17,6 +24,11 @@ public class PlayerController : MonoBehaviour
 
     void Start() {
     
+        currentHealth = 100;
+        fallDamagePerSecond = 20;
+        minFallTime = 2.5f;
+        setCountHealth();
+
         rb = GetComponent<Rigidbody>();
         count = 0;
         setCountText();
@@ -35,7 +47,7 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
 
 
-        if(Input.GetKeyDown("space")) {
+        if(Input.GetKeyDown("space") && grounded) {
             Vector3 jump = new Vector3(0.0f,400.0f,0.0f);
             rb.AddForce(jump);
         }
@@ -44,6 +56,23 @@ public class PlayerController : MonoBehaviour
             rb.transform.position = new Vector3(0f, 0f, 0f);
         }
 
+        CheckFallDamage();
+
+    }
+
+    private void CheckFallDamage(){
+    
+        if(!grounded){
+            currentFallTime += Time.deltaTime;
+        } else {
+            if(currentFallTime > minFallTime){
+                currentHealth -= currentFallTime * fallDamagePerSecond;
+                setCountHealth();
+                checkDead();
+            }
+                currentFallTime = 0;
+        }
+    
     }
 
     void OnTriggerEnter(Collider other) {
@@ -70,6 +99,12 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    void setCountHealth() {
+
+        healthText.text = "Vida: " + Mathf.Clamp(Mathf.Round(currentHealth), 0, 100).ToString() + "/100";
+
+    }
+
     void checkWin() {
 
         if(count >= MAX_POINTS) {
@@ -81,6 +116,19 @@ public class PlayerController : MonoBehaviour
 
         }
     
+    }
+
+    void checkDead() {
+
+        if(currentHealth <= 0) {
+
+            winText.text = "¡Has muerto!";
+            Time.timeScale = 0;
+            //rb.constraints = RigidbodyConstraints.FreezeAll;
+            btRestart.gameObject.SetActive(true);
+
+        }
+
     }
 
     public void onRestartClick() {
@@ -106,6 +154,20 @@ public class PlayerController : MonoBehaviour
 
         }
 
+        
+
     }
-    
+
+    private void OnCollisionStay(Collision collision) {
+
+            grounded = true;
+
+    }
+
+    private void OnCollisionExit(Collision collision) {
+
+            grounded = false;
+
+    }
+
 }
